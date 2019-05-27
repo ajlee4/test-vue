3f598e49b621db0f1cdb1c81e4e51cd96baa8662
ajlee4 <patsyno94@gmail.com>
2019-05-27 18:10:41 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,32 @@
 <template>
   <div id="app" class="app">
     <div class="app__inner">
-    <img alt="picture" src="./assets/component.png" class="app__picture">
-    <div class="app__title">
-      <h2 class="title-text">Beautiful landscape</h2>
-      <button class="btn btn-donate" :disabled='true'>Donate Author</button>
+      <img alt="picture" src="./assets/component.png" class="app__picture">
+      <div class="app__title">
+        <h2 class="title-text">Beautiful landscape</h2>
+        <button class="btn btn-donate" :disabled="true">Donate Author</button>
+      </div>
+
+      <comment-list></comment-list>
     </div>
-   
-    <comment-list></comment-list>
-    </div>
-    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import CommentList from './components/CommentList'
+import HelloWorld from "./components/HelloWorld";
+import CommentList from "./components/CommentList";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     CommentList
   },
   data() {
-    return {
-    
-    }
-   
- 
+    return {};
   },
-   methods: {
-
-    }
- 
-}
+  methods: {}
+};
 </script>
 
 <style lang="scss">
@@ -54,99 +46,95 @@ export default {
 }
 
 .app {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+  height: 100vh;
+  background-color: #f0f0f0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__inner {
     box-sizing: border-box;
-font-family: 'Montserrat', sans-serif;
-height: 100vh;
-background-color: #F0F0F0;
-display: flex;
-justify-content: center;
-align-items: center;
- &__inner {
-   box-sizing: border-box;
-   width: 568px;
-   background-color: #fff;
-   border-radius: 8px;
-   padding: 16px;
- }
- &__picture {
-   max-width: 100%;
- }
- &__title {
-   display: flex;
-   justify-content: space-between;
-   padding-top: 16px;
-   padding-bottom: 16px;
-   .title {
-     &-text {
-       margin: 0;
-       font-weight:400;
-       font-size: 20px;
-       line-height: 24px;
-     }
-   }
- }
+    width: 568px;
+    background-color: #fff;
+    border-radius: 8px;
+    padding: 16px;
+  }
+  &__picture {
+    max-width: 100%;
+  }
+  &__title {
+    display: flex;
+    justify-content: space-between;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    .title {
+      &-text {
+        margin: 0;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 24px;
+      }
+    }
+  }
 }
 .btn {
   &-donate {
     background: linear-gradient(to right, #bec2f3 0%, #f7caeb 160%);
     padding: 8px 16px;
     border-radius: 4px;
-    border:0;
+    border: 0;
     cursor: pointer;
     font-weight: 600;
     font-size: 11px;
     line-height: 14px;
-  
   }
   &-send {
     display: block;
     cursor: pointer;
     font-size: 11px;
-    color:#fff;
+    color: #fff;
     border-radius: 4px;
-    background-color: #2C3BFF;
-    border: 1px solid #2C3BFF;
+    background-color: #2c3bff;
+    border: 1px solid #2c3bff;
     padding: 0px;
-      width: 88px;
-      transition: 0.3s;
-   &.hidden {
-   width: 0px;
-   padding: 0;
-   border: 0;
-   font-size:0;
-  }
+    width: 88px;
+    transition: 0.3s;
+    &.hidden {
+      width: 0px;
+      padding: 0;
+      border: 0;
+      font-size: 0;
+    }
   }
 }
 .comments {
   &-field {
- display: flex;
- justify-content: space-between;
- width: 100%;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
   }
   &-input {
     display: block;
     width: 100%;
-  height: 46px;
-  padding-left: 16px;
-      border-radius: 4px;
-      border: 0.5px solid #707070;
-      padding-top: 0;
-      padding-bottom: 0;
-      box-sizing: border-box;
-    @include placeholder {    
-font-weight: 400;
-font-size: 16px;
-color:#666666;
-line-height: 19px;
+    height: 46px;
+    padding-left: 16px;
+    border-radius: 4px;
+    border: 0.5px solid #707070;
+    padding-top: 0;
+    padding-bottom: 0;
+    box-sizing: border-box;
+    @include placeholder {
+      font-weight: 400;
+      font-size: 16px;
+      color: #666666;
+      line-height: 19px;
     }
-&:focus {
-  border-color:#2C3BFF;
-}
+    &:focus {
+      border-color: #2c3bff;
+    }
   }
 }
-
-
-
 </style>
 
 
--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,143 +1,142 @@
 <template>
-<div class="comments-item " >
-   <template v-if='isShow'>  
-       <div class="item-avatar" >
-                <img src="../assets/av-1.png" alt="">         
-            </div>
-             <div class='item-action' :class="[isVisible ? '' : 'hidden']" >
-                 <span class="edit" @click="editMessage">Edit</span>
-                 <span class="delete" @click="showDelete">Delete</span>
-             </div>
-         <div class="item-info"  @click='showAction'>          
-                <div class="item-info_name">
-                   {{comments.name}}
-                </div>
-                <div class="item-info_text">
-                    {{comments.text}}
-                </div>
+  <div class="comments-item">
+    <template v-if="isShow">
+      <div class="item-avatar">
+        <img src="../assets/av-1.png" alt>
+      </div>
+      <div class="item-action" :class="[isVisible ? '' : 'hidden']">
+        <span class="edit" @click="editMessage">Edit</span>
+        <span class="delete" @click="showDelete">Delete</span>
+      </div>
+      <div class="item-info" @click="showAction">
+        <div class="item-info_name">{{comments.name}}</div>
+        <div class="item-info_text">{{comments.text}}</div>
+      </div>
+    </template>
 
-            </div></template> 
-
-           <div class="delete-field" v-show='isDelete'>
-               <span>Delete message?</span>
-               <button @click='hideDelete'>No</button>
-               <button @click="deleteComment(comments)">Yes</button>
-           </div>
-                  <div class="comments-field" v-show='isEditing' >
-              <input type="text" class="comments-input" placeholder="Write a new comment"  v-model.lazy='comments.text' @blur='hideEditComment(comments)' >
-              <button class="btn btn-send" :class="[isVisible ? '' : 'hidden']" @click='editComment(comments)'>Send</button>
-          </div>
-            </div>
-           
+    <div class="delete-field" v-show="isDelete">
+      <span>Delete message?</span>
+      <button @click="hideDelete">No</button>
+      <button @click="deleteComment(comments)">Yes</button>
+    </div>
+    <div class="comments-field" v-show="isEditing">
+      <input
+        type="text"
+        class="comments-input"
+        placeholder="Write a new comment"
+        v-model.lazy="comments.text"
+        @blur="hideEditComment(comments)"
+      >
+      <button
+        class="btn btn-send"
+        :class="[isVisible ? '' : 'hidden']"
+        @click="editComment(comments)"
+      >Send</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props:['comments',],
+  props: ["comments"],
 
-    data(){
-        return {
-             isVisible:false,
-             isDelete:false,
-             isEditing:false,
-             isShow:true,
-        }
+  data() {
+    return {
+      isVisible: false,
+      isDelete: false,
+      isEditing: false,
+      isShow: true
+    };
+  },
+  methods: {
+    showAction(e) {
+      this.isVisible = !this.isVisible;
     },
-    methods: {
-        showAction(e){
-            this.isVisible=!this.isVisible;   
-        },
-        deleteComment(comments){
-       this.$emit('delete-comment', comments);
-       this.isDelete = !this.isDelete;
-       this.isShow = !this.isShow;
-       this.isVisible = !this.isVisible;
-        },
-        showDelete(){         
-            this.isDelete=true;    
-            this.isShow = false;   
-        },
-        hideDelete() {
-            this.isDelete = false;
-            this.isShow = true;           
-        },
-        editMessage(){
-            this.isEditing = true;
-            this.isDelete =false;
-            this.isShow = false;
-        },
-        editComment(){
-            this.isShow = true;
-             this.isVisible= false;
-            this.isDelete =false;
-             this.isEditing = false;
-        },
-        hideEditComment() {
-            this.isVisible= false;
-            this.isShow = true;
-            this.isEditing=false;
-           
-        },
+    deleteComment(comments) {
+      this.$emit("delete-comment", comments);
+      this.isDelete = !this.isDelete;
+      this.isShow = !this.isShow;
+      this.isVisible = !this.isVisible;
+    },
+    showDelete() {
+      this.isDelete = true;
+      this.isShow = false;
+    },
+    hideDelete() {
+      this.isDelete = false;
+      this.isShow = true;
+    },
+    editMessage() {
+      this.isEditing = true;
+      this.isDelete = false;
+      this.isShow = false;
+    },
+    editComment() {
+      this.isShow = true;
+      this.isVisible = false;
+      this.isDelete = false;
+      this.isEditing = false;
+    },
+    hideEditComment() {
+      this.isVisible = false;
+      this.isShow = true;
+      this.isEditing = false;
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 .comments {
-    &-item {
-        padding: 10px 0px;
-        display: flex;
-        font-size: 15px;
-        align-items: center;
-        max-width: 100%;
-        overflow: hidden;
-        transition: 0.3s;
-&.delete{
-display:none;
-}
-        .item {
-        &-avatar {
-            margin-right: 20px;
-        }
-         &-info {
-             display: block;
-             width: 100%;
-             transition: 0.3s;
-           
-            &_name {
-                font-size: 11px;
-            }
-            &_text {
-                font-size: 16px;
-                white-space: nowrap;
-               
-            }
-        }
-        &-action {
-            font-size: 11px;
-            font-weight: 600;
-            transition: 0.3s;
-            width: 100px;
-            &.hidden {
-                width: 0;
-                font-size: 0;
-            }
-            span {
-                padding: 5px;
-                cursor: pointer;
-            }
-            .edit{
-                color:#2C3BFF;
-                
-            }
-            .delete{
-                color:#FF0000;
-              
-            }
-        }
-}
+  &-item {
+    padding: 10px 0px;
+    display: flex;
+    font-size: 15px;
+    align-items: center;
+    max-width: 100%;
+    overflow: hidden;
+    transition: 0.3s;
+    &.delete {
+      display: none;
     }
+    .item {
+      &-avatar {
+        margin-right: 20px;
+      }
+      &-info {
+        display: block;
+        width: 100%;
+        transition: 0.3s;
 
+        &_name {
+          font-size: 11px;
+        }
+        &_text {
+          font-size: 16px;
+          white-space: nowrap;
+        }
+      }
+      &-action {
+        font-size: 11px;
+        font-weight: 600;
+        transition: 0.3s;
+        width: 100px;
+        &.hidden {
+          width: 0;
+          font-size: 0;
+        }
+        span {
+          padding: 5px;
+          cursor: pointer;
+        }
+        .edit {
+          color: #2c3bff;
+        }
+        .delete {
+          color: #ff0000;
+        }
+      }
+    }
+  }
 }
 </style>
